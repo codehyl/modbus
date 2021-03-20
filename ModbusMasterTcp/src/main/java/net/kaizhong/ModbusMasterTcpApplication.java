23efde955183392d975109f8259ffb045ee25655
H-Y-L <codehyl@qq.com>
2021-03-20 09:14:21 +0800

    Initial commit

---
+++ ModbusMasterTcp/src/main/java/net/kaizhong/ModbusMasterTcpApplication.java
@@ -0,0 +1,19 @@
+package net.kaizhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version V1.0
+ * @Package net.kaizhong
+ * @Description
+ * @Author h-y-l
+ * @Date 2021/3/20 7:07 上午
+ */
+@SpringBootApplication
+public class ModbusMasterTcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModbusMasterTcpApplication.class, args);
+    }
+}
---
+++ modbus4j/src/main/java/net/kaizhong/Modbus4jApplication.java
@@ -0,0 +1,20 @@
+package net.kaizhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version V1.0
+ * @Package net.kaizhong
+ * @Description
+ * @Author h-y-l
+ * @Date 2021/3/20 8:05 上午
+ */
+@SpringBootApplication
+public class Modbus4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modbus4jApplication.class, args);
+    }
+
+}
