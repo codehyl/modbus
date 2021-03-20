cd9ee2939b7f0e8558d4be0b48e0d7f7dda1fec4
codehyl <codehyl@qq.com>
2021-03-20 13:05:17 +0800

    Initial commit

--- modbus4j/src/main/java/net/kaizhong/Modbus4jApplication.java
+++ modbus4j/src/main/java/net/kaizhong/Modbus4jApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Modbus4jApplication {
 
     public static void main(String[] args) {
+//        1
         SpringApplication.run(Modbus4jApplication.class, args);
     }
 
